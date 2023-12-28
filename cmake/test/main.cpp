990c3f44b0f040b6f9221c9e4092573569eacd30
lengyuanjian <1181444531@qq.com>
2023-12-28 19:27:53 +0800

    add version

--- cmake/test/main.cpp
+++ cmake/test/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-
+#include "ver.h"
 int main(int argc, char * argv[])
 {
+    std::cout << "MyProject Version: " << MYPROJECT_VERSION_MAJOR << "." << MYPROJECT_VERSION_MINOR << std::endl;
+    std::cout<< GIT_VERSION << "\n";
+    std::cout<< BUILD_DATE << "\n";
     for(int i = 0; i < argc; ++i)
     {
         std::cout<<"["<<argv[i]<<"]\n";
